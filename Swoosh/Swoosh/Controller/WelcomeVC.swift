9fee0a96490127e2f33c32ff67f6a143f241a881
Sadoed <barmoza999@gmail.com>
2019-02-26 23:30:38 +0300

    add: seguу and maketed the MVC

--- Swoosh/Swoosh/Controller/WelcomeVC.swift
+++ Swoosh/Swoosh/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
     
@@ -20,9 +20,9 @@ class ViewController: UIViewController {
         bgImg.frame = view.frame
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
-
+    
 }
 
